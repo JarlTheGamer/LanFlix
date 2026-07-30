b6f88623cbc187ef69c7db3e174499c4b981bc81
JarlTheGamer <jarlvanniel@gmail.com>
2026-07-31 01:45:53 +0200

    release: Publish Lanflix OTA update v1.3.8 - Lanflix v1.3.8 release package

--- build-tools/AndroidVersions/native-app/app/build.gradle.kts
+++ build-tools/AndroidVersions/native-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lanflix.webview"
         minSdk = 21
         targetSdk = 34
-        versionCode = 37
-        versionName = "1.3.7"
+        versionCode = 38
+        versionName = "1.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
