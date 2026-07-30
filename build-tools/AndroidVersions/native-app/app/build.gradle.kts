9984f64509f04f1abb88555f61f2be02c6137f6a
JarlTheGamer <jarlvanniel@gmail.com>
2026-07-31 01:50:39 +0200

    release: Publish Lanflix OTA update v1.4.0 - v1.4.0 - Unified update modal for server and Android app

--- build-tools/AndroidVersions/native-app/app/build.gradle.kts
+++ build-tools/AndroidVersions/native-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lanflix.webview"
         minSdk = 21
         targetSdk = 34
-        versionCode = 39
-        versionName = "1.3.9"
+        versionCode = 40
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
